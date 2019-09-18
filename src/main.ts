00ca21273790b3ed276fbe15dd6ec8050aa2eb25
Aryan Jabbari <AryanJabbari@gmail.com>
2019-09-18 17:04:54 -0400

    Use PORT environment variable to support Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
